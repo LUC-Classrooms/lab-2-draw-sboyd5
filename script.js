e66c07e3f9066d49d421fa0d7ca4994882d54838
sboyd5 <157087132+sboyd5@users.noreply.github.com>
2024-02-19 17:04:49 +0000

    Lab 2 - Draw Completed

--- script.js
+++ script.js
@@ -5,14 +5,14 @@ function setup(){
 function draw(){
   background(200, 200, 244);
   // add your drawing code here ...
-Fill(200)
+fill(200)
   noStroke();
   triangle(width/2 - 45, height/2 - 55, width/2 - 25, height/2 - 40, width/2 - 45, height/2 - 20);
   triangle(width/2 + 45, height/2 - 55, width/2 + 25, height/2 - 40, width/2 + 45, height/2 - 20);
   ellipse(width/2, height/2, 100); //head
   fill("blue");
-  ellipse(width/ - 20, height/2 - 10, 10);
-  ellipse(width/ + 20, height/2 - 10, 10);
+  ellipse(width/ - 2, height/2 - 10, 10);
+  ellipse(width/ + 2, height/2 - 10, 10);
   fill("orange");
 triangle(width/2 - 5, height/2, width/2 + 5, height/2, width/2, height/2 + 8);
 stroke(255);
@@ -24,7 +24,7 @@ line(width/2 + 10, height/2 + 5, width/2 + 35, height/2 + 5);
 line(width/2 - 10, height/2 + 5, width/2 - 30, height/2 + 10);
 line(width/2 + 10, height/2 + 5, width/2 + 30, height/2 + 10);
 nofill();
-Arc(width/2 - 10, height/2 + 12, 20, 20, 0, PI);
-Arc(width/2 + 10, height/2 + 12, 20, 20, 0, PI);
+arc(width/2 - 10, height/2 + 12, 20, 20, 0, PI);
+arc(width/2 + 10, height/2 + 12, 20, 20, 0, PI);
   
 }
